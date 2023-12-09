f9cee714af33953e35f1f0cf2d01756d911cafea
iamseeley <seeleypersonal@gmail.com>
2023-12-09 15:03:34 -0500

    re-added tailwindcss prose and added formatted date to posts

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -3,6 +3,20 @@ import { notFound } from "next/navigation";
 import { Metadata } from "next";
 import { H2 } from "@/app/components/Typography/TypographyHeading";
 import { Mdx } from "@/app/components/Mdx/Mdx";
+import Text from "@/app/components/Typography/TypographyText";
+
+function formatDate(dateString: string): string {
+  // Split the date string into parts
+  const [year, month, day] = dateString.split('-').map(part => parseInt(part, 10));
+
+  // Create a new date object using local time
+  const date = new Date(year, month - 1, day);
+
+  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+}
+
+
 
 export async function generateMetadata({
     params,
@@ -46,9 +60,15 @@ export default function Post({ params }) {
   if (!post) {
     notFound();
   }
+
+  const formattedDate = formatDate(post.metadata.date);
+
     return (
         <section>
+          <div className="mb-6">
             <H2>{post.metadata.title}</H2>
+            <Text color="tertiary" size={3}>{formattedDate}</Text>
+          </div>
             <article className="prose prose-headings:mt-4 text-type-primary">
                 <Mdx source={post.content} />
             </article>
