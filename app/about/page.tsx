28d46055b8c3b8a2b2f1f1098a5412f68b5933f4
iamseeley <seeleypersonal@gmail.com>
2023-12-18 19:07:03 -0500

    working on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -36,13 +36,13 @@ export default function About() {
                 <Text size={2} spaced color='primary'>None of us knew how to code, so we didn&apos;t have a way to test our ideas. I was tired of creating business model after business model, I wanted to build actual products. I started to self-teach myself web development with the goal of building some of our ideas.</Text>
             </div>
             <div className='mb-4'>
-                <Text size={2} spaced color='primary'>My junior year summer I developed the front end for an online therapy platform that me and a few peers created. It was a part of an Entrepreneurship program in Europe and at the end we pitched our product to investors!</Text>
+                <Text size={2} spaced color='primary'>My junior year summer, I developed the front end for an online therapy platform that me and a few peers created. It was a part of an Entrepreneurship program in Europe, and at the end we pitched our product to investors!</Text>
             </div>
             <div className='mb-4'>
-                <Text size={2} spaced color='primary'>I fell deep down the web development rabbit hole and that is where I&apos;ve been living ever since.</Text>
+                <Text size={2} spaced color='primary'>I fell deep down the web development rabbit hole, and that is where I&apos;ve been living ever since.</Text>
             </div>
             <div className='mb-4'>
-                <Text size={2} spaced color='primary'>Since that summer I have completed multiple online software and web development courses. I also started providing web development services to small businesses.</Text>
+                <Text size={2} spaced color='primary'>Since that summer, I have completed multiple online software and web development courses. I also started providing web development services to small businesses.</Text>
             </div>
             
 
